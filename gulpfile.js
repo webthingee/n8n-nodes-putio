da0789cacfa172c095b85aae191e1e3680529194
Sean Lange <sean@seanlange.com>
2025-04-10 09:05:16 -0700

    Update gulpfile for simpler icon building

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 const gulp = require('gulp');
-const { spawn } = require('child_process');
 
-gulp.task('build:icons', function (cb) {
-	const cmd = spawn('node', ['./node_modules/@n8n/nodes-base/dist/scripts/build-icons.js'], {
-		stdio: 'inherit',
-	});
-	cmd.on('close', () => {
-		cb();
-	});
+gulp.task('build:icons', function() {
+	return gulp.src('icons/*.svg')
+		.pipe(gulp.dest('dist/icons'));
 }); 
