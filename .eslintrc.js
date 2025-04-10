6e2e0e4a2653c2d6385e01695636094a76475e91
Sean Lange <sean@seanlange.com>
2025-04-10 09:18:24 -0700

    Update package.json scripts and fix CommonJS compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	extends: ['plugin:@typescript-eslint/recommended'],
+	ignorePatterns: ['dist/**/*'],
 	rules: {
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-var-requires': 'off',
